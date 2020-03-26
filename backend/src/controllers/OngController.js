c129fb6ad325c15e85fb06de6fdbec468ec092de
Ricardo Camilo <ricardo_hks@hotmail.com>
2020-03-25 23:05:00 -0300

    Front-end do app

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,30 +1,28 @@
-const crypt = require('crypto');
+const crypto = require('crypto');
 const connection = require('../database/connection');
 /*importa a conexão com o banco de dados*/
 
 module.exports = {
-
     async index (request, response) {
         const ongs = await connection('ongs').select('*');
     
         return response.json(ongs);
     },
 
-    async create(request, responde){
+    async create(request, response){
         const {name, email, whatsapp, city, uf} = request.body;
 
-        const id = crypt.randomBytes(4).toString('HEX');
+        const id = crypto.randomBytes(4).toString('HEX');
         /*gera 4 bity de caracteres aleatorios e converte pra um string do tipo hexadecimal*/
-    
         await connection('ongs').insert({
             id,
-            name,email,
+            name,
+            email,
             whatsapp,
             city,
             uf,
         })
 
-        return Response.json({ id });
-
+        return response.json({ id });
     }
 };
